b2a14a899c63f4ea94f99360cd6da4d6e59b6064
Fitri Wibowo <parcoxs@gmail.com>
2014-01-07 11:54:59 +0700

    Initial commit

---
+++ brightness-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CURRENT_BRIGHTNESS=`brightness -l | grep bright |  cut -d " " -f 4`
+echo "Current brightness = $CURRENT_BRIGHTNESS"
+NEXT_BRIGHTNESS=`echo $CURRENT_BRIGHTNESS+0.1 | bc`
+if [[ $(echo "$NEXT_BRIGHTNESS > 1" | bc) -eq "1" ]]
+then
+	NEXT_BRIGHTNESS=1
+fi
+echo "Next brightness = $NEXT_BRIGHTNESS"
+/usr/bin/brightness $NEXT_BRIGHTNESS
